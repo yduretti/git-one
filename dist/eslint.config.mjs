f2d4b448eaecbb40ce7865737ca359105768e880
yduretti <31548791+yduretti@users.noreply.github.com>
2025-02-14 18:53:53 -0300

    Develop (#8)
    * code change
    
    * teste
    
    * teste
    
    * prettier
    
    * prettier
    
    * jest
    
    * jest
    
    * build
    
    * build 2t

---
+++ dist/eslint.config.mjs
@@ -0,0 +1,9 @@
+"use strict";
+import globals from "globals";
+import typeScriptEslint from "typescript-eslint"; // Atenção: esse pacote não é o padrão da comunidade
+export default {
+    files: ["**/*.js"],
+    languageOptions: {
+        sourceType: "script",
+    },
+};
---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("<h1>Hello YD</h1>");
+});
+app.listen(3000, () => {
+    console.log("Started on port 3000");
+});
