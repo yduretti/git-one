07d22204b9d76b1deab1ec173e5bd5a1678df732
yduretti <yuriduretti@gmail.com>
2025-02-17 08:18:40 -0300

    ingress working in k3d cluster

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   const mySecret = process.env.MY_SECRET;
   console.log('The secret is:', mySecret);
 
-  let resultado = "<h1 >Hello YD </h1><br><h2> secret: " + mySecret + " </h2><br> hostname: " + os.hostname()
+  let resultado = "<h1 >FULL STACK TEST COMPLETED YD </h1><br><h2> secret: " + mySecret + " </h2><br> hostname: " + os.hostname()
   res.send(resultado);
   //res.json({ "celsius": celsius, "maquina": os.hostname() });
 });
